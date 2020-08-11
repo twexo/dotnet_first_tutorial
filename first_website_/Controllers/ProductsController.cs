72a14dd587d0861692370af73b00b795f7ca1a54
adiupm <adiupm@gmail.com>
2020-08-11 22:33:14 +0300

    first commit tutorial done

---
+++ first_website_/Controllers/ProductsController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using first_website_.Models;
+using first_website_.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+namespace first_website_.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(JsonFileProductService productService)
+        {
+            this.ProductService = productService;
+            
+        }
+
+        public JsonFileProductService ProductService { get; }
+
+        [Route("/Products")]
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            Console.WriteLine("Getting those shits  ");
+            return ProductService.GetProducts();
+        }
+
+
+        [Route("/Rate")]
+        [HttpGet]
+        public ActionResult Gett( [FromQuery] string ProductId, [FromQuery] int Rating)
+        {
+            Console.WriteLine(ProductId + " and " + Rating);
+            ProductService.AddRating(ProductId, Rating);
+            return Ok();
+            
+        }
+    }
+
+
+}
---
+++ first_website_/Pages/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using first_website_.Models;
+using first_website_.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace first_website_.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public JsonFileProductService ProductService;
+        public IEnumerable<Product> Products { get; private set; }
+        public IndexModel(
+            ILogger<IndexModel> logger,
+            JsonFileProductService productService
+            )
+        {
+            ProductService = productService;
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            Products = ProductService.GetProducts();
+        }
+    }
+}
